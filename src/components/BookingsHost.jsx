faeaba048d0ca1dcb4e416fe00f6a00a8ac1b41a
sam <saminusama@gmail.com>
2024-05-07 05:22:38 -0400

    bookingshost

--- src/components/BookingsHost.jsx
+++ src/components/BookingsHost.jsx
@@ -1,3 +1,21 @@
-const BookingsHost = () => {};
+import { useContext, useEffect } from "react";
+import { useImmer } from "use-immer";
+import { getHostBookings } from "../api/bookings";
+
+const BookingsHost = () => {
+  const user = useContext("user");
+  const [bookings, updateBookings] = useImmer(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data =getHostBookings(user.id);
+      updateBookings((bookings) => data);
+    };
+
+    fetch();
+  });
+
+  return <ol>{bookings && bookings.map((b) => <li key={b.id}>{b.id}</li>)}</ol>;
+};
 
 export default BookingsHost;
